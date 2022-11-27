1cf26201b650e8aa2448dd3bd5f30832a4071731
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-27 17:23:17 +0100

    use Redux for manage the store app

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,23 +1,34 @@
+import {useDispatch} from 'react-redux';
+import {cartActions} from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
-
-  return (
-    <li className={classes.item}>
-      <Card>
-        <header>
-          <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
-        </header>
-        <p>{description}</p>
-        <div className={classes.actions}>
-          <button>Add to Cart</button>
-        </div>
-      </Card>
-    </li>
-  );
+	const {name, price, description, id} = props;
+	const dispatch = useDispatch();
+	const addProductHandler = () => {
+		dispatch(
+			cartActions.addItemToCart({
+				id,
+				name: name,
+				price,
+			})
+		);
+	};
+	return (
+		<li className={classes.item}>
+			<Card>
+				<header>
+					<h3>{name}</h3>
+					<div className={classes.price}>${price.toFixed(2)}</div>
+				</header>
+				<p>{description}</p>
+				<div className={classes.actions}>
+					<button onClick={addProductHandler}>Add to Cart</button>
+				</div>
+			</Card>
+		</li>
+	);
 };
 
 export default ProductItem;
